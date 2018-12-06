c04a27f84fd47bbd595c32bbef295948be87faea
Noah Adams <36975465+SirWashnerQuant@users.noreply.github.com>
2018-12-05 23:03:05 -0500

    Added changes to material change

---
+++ Assets/Scripts/ChangeMetalScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMetalScript : MonoBehaviour {
+
+    public GameObject player;
+    public Rigidbody rb;
+    public Material mat;
+    public Material playerMat;
+   
+
+	// Use this for initialization
+	void Start () {
+        rb = player.GetComponent<Rigidbody>();
+        playerMat = player.GetComponent<Material>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+       
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        player.GetComponent<Renderer>().material = mat;
+        player.GetComponent<Rigidbody>().mass = 1.5f;
+        Debug.Log("jeff");
+    }
+}
